aa146f6d27f779bff6e112282a273dcf7126ab99
aninerd <aninerd@users.noreply.github.com>
2024-06-19 13:55:06 +0000

    deploy: 99409be135b2b3c333ced78f0499977a594a3c39

---
+++ precache-manifest.fb7c703370d01f94d17d37f1c9efa8ef.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1169ddbda69e5b8033f3dd1e982f9533",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9ee9f99cacdc0c01d132",
+    "url": "./static/css/2.232cac0f.chunk.css"
+  },
+  {
+    "revision": "b61b8fa0cdc56174e99b",
+    "url": "./static/css/3.fa1beb1f.chunk.css"
+  },
+  {
+    "revision": "6d834badbd5178dd4ba5",
+    "url": "./static/css/main.096c7372.chunk.css"
+  },
+  {
+    "revision": "9ee9f99cacdc0c01d132",
+    "url": "./static/js/2.dbb337ff.chunk.js"
+  },
+  {
+    "revision": "610f6619e8cd17771efceae1234b3de0",
+    "url": "./static/js/2.dbb337ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b61b8fa0cdc56174e99b",
+    "url": "./static/js/3.9c0e3c95.chunk.js"
+  },
+  {
+    "revision": "6deb9ada2d2a90964ebad279404033bd",
+    "url": "./static/js/3.9c0e3c95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aab124443efa043b627",
+    "url": "./static/js/4.73404abf.chunk.js"
+  },
+  {
+    "revision": "6d834badbd5178dd4ba5",
+    "url": "./static/js/main.b97b0a49.chunk.js"
+  },
+  {
+    "revision": "51ad08054ab304d5371e",
+    "url": "./static/js/runtime-main.1e7f02ed.js"
+  }
+]);
